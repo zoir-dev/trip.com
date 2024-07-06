59bca607afe5d3edb4f3f59e3c76518c99919001
Sultanov Abdurazzoq <abdurazzoqsultanov2208@gmail.com>
2024-07-06 15:32:30 +0500

    feat(flights form done)

--- components/home/search-form/search-flights/multi-city.tsx
+++ components/home/search-form/search-flights/multi-city.tsx
@@ -1,8 +1,13 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { cn } from '@/lib/utils';
+import { format } from "date-fns";
+import { CalendarIcon } from 'lucide-react';
+import { useTranslations } from "next-intl";
+import { FormEvent, useState } from 'react';
+import { z } from 'zod';
 
 interface Flight {
     id: number;
@@ -22,12 +27,25 @@ const cityOptions = [
     { value: 'Tokyo', label: 'Tokyo' },
     { value: 'Dubai', label: 'Dubai' },
     { value: 'Singapore', label: 'Singapore' },
-    // Add more cities as needed
 ];
 
-const MultiCityForm = () => {
+const flightSchema = z.object({
+    from: z.string().nonempty("Please select a departure city."),
+    to: z.string().nonempty("Please select a destination city."),
+    date: z.string().nonempty("Please select a date."),
+});
+
+type FlightErrors = Record<keyof Omit<Flight, 'id'>, string>;
+
+interface MultiCityFormProps {
+    searchPath: (path: string) => void; // Update this line
+}
+
+const MultiCityForm = ({ searchPath }: MultiCityFormProps) => {
+    const t = useTranslations('');
     const [flights, setFlights] = useState<Flight[]>([{ id: 1, from: '', to: '', date: '' }]);
-    const router = useRouter();
+    const [errors, setErrors] = useState<Record<number, FlightErrors>>({});
+
 
     const addFlight = () => {
         const lastFlight = flights[flights.length - 1];
@@ -40,37 +58,64 @@ const MultiCityForm = () => {
         ));
     };
 
-    const searchFlights = () => {
-        const path = flights
-            .map((flight) => `${flight.from.toLocaleLowerCase()}-to-${flight.to.toLocaleLowerCase()}-${flight.date}`)
-            .join('&');
-        router.push(`/flights/${path}`);
+    const handleDateChange = (id: number, date: Date | undefined) => {
+        const formattedDate = date ? format(date, 'yyyy-MM-dd') : '';
+        handleInputChange(id, 'date', formattedDate);
+    };
+
+    const validateFlights = (): boolean => {
+        const newErrors: Record<number, FlightErrors> = {};
+
+        flights.forEach((flight) => {
+            const result = flightSchema.safeParse(flight);
+            if (!result.success) {
+                newErrors[flight.id] = { from: '', to: '', date: '' };
+                result.error.errors.forEach((error) => {
+                    const field = error.path[0] as keyof FlightErrors;
+                    newErrors[flight.id][field] = error.message;
+                });
+            }
+        });
+
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+
+    const searchFlights = (evt: FormEvent<HTMLFormElement>) => {
+        evt.preventDefault();
+        if (!validateFlights()) return;
+
+        const path = flights.map((flight) => `${flight.from.toLocaleLowerCase()}-to-${flight.to.toLocaleLowerCase()}-${flight.date}`).join('&');
+        searchPath(path); // Call the function with the path argument
     };
 
     return (
-        <div className="max-w-2xl mx-auto p-4">
-            <h2 className="text-2xl font-bold mb-4">Multi-City Flight Search</h2>
-            <form>
+        <div className="my-4">
+            <h2 className="text-2xl font-bold mb-4 sr-only">Multi-City Flight Search</h2>
+            <form onSubmit={(evt) => searchFlights(evt)}>
                 {flights.map((flight) => (
                     <div key={flight.id} className="mb-4 p-4 border rounded-lg">
-                        <div className="grid grid-cols-3 gap-4 mb-2">
+                        <div className="grid grid-cols-3 gap-4 mb-2 max-[690px]:grid-cols-1">
                             <div>
                                 <Select
                                     value={flight.from}
                                     onValueChange={(value) => handleInputChange(flight.id, 'from', value)}
                                     required
                                 >
-                                    <SelectTrigger className="w-[180px]">
+                                    <SelectTrigger className="">
                                         <SelectValue placeholder="From" />
                                     </SelectTrigger>
                                     <SelectContent>
                                         <SelectGroup>
                                             {cityOptions.map((city) => (
-                                                <SelectItem key={city.value} value={city.value}>{city.label}</SelectItem>
+                                                <SelectItem key={city.value} value={city.value}>
+                                                    {t(city.label.charAt(0).toUpperCase() + city.label.slice(1))}
+                                                </SelectItem>
                                             ))}
                                         </SelectGroup>
                                     </SelectContent>
                                 </Select>
+                                {errors[flight.id]?.from && <p className="text-red-500">{errors[flight.id]?.from}</p>}
                             </div>
                             <div>
                                 <Select
@@ -78,44 +123,64 @@ const MultiCityForm = () => {
                                     onValueChange={(value) => handleInputChange(flight.id, 'to', value)}
                                     required
                                 >
-                                    <SelectTrigger className="w-[180px]">
+                                    <SelectTrigger className="">
                                         <SelectValue placeholder="To" />
                                     </SelectTrigger>
                                     <SelectContent>
                                         <SelectGroup>
                                             {cityOptions.map((city) => (
-                                                <SelectItem key={city.value} value={city.value}>{city.label}</SelectItem>
+                                                <SelectItem key={city.value} value={city.value}>
+                                                    {city.label}
+                                                </SelectItem>
                                             ))}
                                         </SelectGroup>
                                     </SelectContent>
                                 </Select>
+                                {errors[flight.id]?.to && <p className="text-red-500">{errors[flight.id]?.to}</p>}
                             </div>
                             <div>
-                                <Input
-                                    type="date"
-                                    value={flight.date}
-                                    onChange={(e) => handleInputChange(flight.id, 'date', e.target.value)}
-                                    className="w-full p-2 border rounded"
-                                    required
-                                />
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <Button
+                                            variant={"outline"}
+                                            className={cn(
+                                                "w-full justify-start text-left font-normal",
+                                                !flight.date && "text-muted-foreground"
+                                            )}
+                                        >
+                                            <CalendarIcon className="mr-2 h-4 w-4" />
+                                            {flight.date ? format(new Date(flight.date), "PPP") : <span>Pick a date</span>}
+                                        </Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-auto p-0" align="start">
+                                        <Calendar
+                                            mode="single"
+                                            selected={flight.date ? new Date(flight.date) : undefined}
+                                            onSelect={(date) => handleDateChange(flight.id, date)}
+                                            initialFocus
+                                        />
+                                    </PopoverContent>
+                                </Popover>
+                                {errors[flight.id]?.date && <p className="text-red-500">{errors[flight.id]?.date}</p>}
                             </div>
                         </div>
                     </div>
                 ))}
-                <div className="flex space-x-4">
+                <div className="flex space-x-4 w-full justify-between">
                     {flights.length < 5 && (
                         <Button
+                            variant={'default'}
                             type="button"
                             onClick={addFlight}
-                            className="mt-4 bg-blue-500 text-white p-2 rounded"
+                            className="mt-4 w-1/3 text-white p-2 rounded max-[690px]:w-full"
                         >
                             Add Flight
                         </Button>
                     )}
                     <Button
-                        type="button"
-                        onClick={searchFlights}
-                        className="mt-4 bg-green-500 text-white p-2 rounded"
+                        variant={'outline'}
+                        type="submit"
+                        className="mt-4 w-2/5 p-2 rounded max-[690px]:w-full"
                     >
                         Search
                     </Button>
---
+++ components/home/search-form/search-flights/round-trip.tsx
@@ -0,0 +1,161 @@
+import { FormEvent, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+import { format } from "date-fns";
+import { CalendarIcon } from 'lucide-react';
+import { Calendar } from '@/components/ui/calendar';
+import { useTranslations } from "next-intl";
+import { z } from 'zod';
+
+const cityOptions = [
+    { value: 'New York', label: 'New York' },
+    { value: 'Los Angeles', label: 'Los Angeles' },
+    { value: 'Chicago', label: 'Chicago' },
+    { value: 'Houston', label: 'Houston' },
+    { value: 'Phoenix', label: 'Phoenix' },
+    { value: 'London', label: 'London' },
+    { value: 'Paris', label: 'Paris' },
+    { value: 'Tokyo', label: 'Tokyo' },
+    { value: 'Dubai', label: 'Dubai' },
+    { value: 'Singapore', label: 'Singapore' },
+];
+
+const flightSchema = z.object({
+    from: z.string().nonempty("Please select a departure city."),
+    to: z.string().nonempty("Please select a destination city."),
+    date: z.string().nonempty("Please select a date."),
+});
+
+type FlightErrors = Record<keyof Omit<{ from: string, to: string, date: string }, 'id'>, string>;
+
+
+const RoundTrip = ({ searchPath }: { searchPath: (path: string) => void }) => {
+    const t = useTranslations('');
+    const [flight, setFlight] = useState<{ from: string, to: string, date: string }>({ from: '', to: '', date: '' });
+    const [errors, setErrors] = useState<FlightErrors>({ from: '', to: '', date: '' });
+
+    const handleInputChange = (field: keyof { from: string, to: string, date: string }, value: string) => {
+        setFlight({ ...flight, [field]: value });
+    };
+
+    const handleDateChange = (date: Date | undefined) => {
+        const formattedDate = date ? format(date, 'yyyy-MM-dd') : '';
+        handleInputChange('date', formattedDate);
+    };
+
+    const validateFlight = (): boolean => {
+        const result = flightSchema.safeParse(flight);
+        if (!result.success) {
+            const newErrors: FlightErrors = { from: '', to: '', date: '' };
+            result.error.errors.forEach((error) => {
+                const field = error.path[0] as keyof FlightErrors;
+                newErrors[field] = error.message;
+            });
+            setErrors(newErrors);
+            return false;
+        }
+        setErrors({ from: '', to: '', date: '' });
+        return true;
+    };
+
+    const searchFlight = (evt: FormEvent<HTMLFormElement>) => {
+        evt.preventDefault();
+        if (!validateFlight()) return;
+
+        const path = `${flight.from.toLocaleLowerCase()}-to-${flight.to.toLocaleLowerCase()}-${flight.date}`;
+
+        searchPath(path);
+    };
+
+    return (
+        <div className="my-4">
+            <h2 className="text-2xl font-bold mb-4 sr-only">One-Way Flight Search</h2>
+            <form onSubmit={(evt) => searchFlight(evt)}>
+                <div className="mb-4 p-4 border rounded-lg">
+                    <div className="grid grid-cols-4 gap-4 mb-2">
+                        <div>
+                            <Select
+                                value={flight.from}
+                                onValueChange={(value) => handleInputChange('from', value)}
+                                required
+                            >
+                                <SelectTrigger className="">
+                                    <SelectValue placeholder="From" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        {cityOptions.map((city) => (
+                                            <SelectItem key={city.value} value={city.value}>
+                                                {t(city.label.charAt(0).toUpperCase() + city.label.slice(1))}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                            {errors.from && <p className="text-red-500">{errors.from}</p>}
+                        </div>
+                        <div>
+                            <Select
+                                value={flight.to}
+                                onValueChange={(value) => handleInputChange('to', value)}
+                                required
+                            >
+                                <SelectTrigger className="">
+                                    <SelectValue placeholder="To" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        {cityOptions.map((city) => (
+                                            <SelectItem key={city.value} value={city.value}>
+                                                {city.label}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                            {errors.to && <p className="text-red-500">{errors.to}</p>}
+                        </div>
+                        <div>
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <Button
+                                        variant={"outline"}
+                                        className={cn(
+                                            "w-full justify-start text-left font-normal",
+                                            !flight.date && "text-muted-foreground"
+                                        )}
+                                    >
+                                        <CalendarIcon className="mr-2 h-4 w-4" />
+                                        {flight.date ? format(new Date(flight.date), "PPP") : <span>Pick a date</span>}
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-auto p-0" align="start">
+                                    <Calendar
+                                        mode="single"
+                                        selected={flight.date ? new Date(flight.date) : undefined}
+                                        onSelect={(date) => handleDateChange(date)}
+                                        initialFocus
+                                    />
+                                </PopoverContent>
+                            </Popover>
+                            {errors.date && <p className="text-red-500">{errors.date}</p>}
+                        </div>
+                        <div className="flex space-x-4">
+                            <Button
+                                variant={'default'}
+                                type="submit"
+                                className="p-2 rounded w-full"
+                            >
+                                Search
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default RoundTrip;
