a3b66c8d9a96a97de962c2a86b233a064c69bf86
ayubxon13 <yuldoshevv7@gmail.com>
2024-07-23 19:39:50 +0500

    fixed hydration error

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,32 +6,35 @@ import { useState } from "react";
 import { useTranslations } from "next-intl";
 import SearchForm from "@/components/home/search-form";
 import Recommendations from "@/components/home/recommendations";
+import NoSSR from 'react-no-ssr';  // Import NoSSR
 
 export default function Home() {
   const [tab, setTab] = useState('flight')
 
   const t = useTranslations()
   return (
-    <div className="overflow-y-auto max-w-full">
-      <Image
-        src={flight}
-        alt="flight"
-        fill
-        className={`object-cover bg-bottom w-full !h-[72vh] z-0 fixed top-0 opacity-0 duration-700 ${tab === 'flight' && 'opacity-100'}`}
-      />
-      <Image
-        src={hotel}
-        alt="hotel"
-        fill
-        className={`object-cover bg-bottom w-full !h-[72vh] z-0 fixed top-0 opacity-0 duration-700 ${tab === 'hotel' && 'opacity-100'}`}
-      />
-      <div className="relative">
-        <SearchForm tab={tab} setTab={setTab} />
-        <div className="w-full bg-white min-h-screen rounded-t-[32px] pt-10 px-4">
-          <Recommendations />
+    <NoSSR>
+      <div className="overflow-y-auto max-w-full">
+        <Image
+          src={flight}
+          alt="flight"
+          fill
+          className={`object-cover bg-bottom w-full !h-[72vh] z-0 fixed top-0 opacity-0 duration-700 ${tab === 'flight' && 'opacity-100'}`}
+        />
+        <Image
+          src={hotel}
+          alt="hotel"
+          fill
+          className={`object-cover bg-bottom w-full !h-[72vh] z-0 fixed top-0 opacity-0 duration-700 ${tab === 'hotel' && 'opacity-100'}`}
+        />
+        <div className="relative">
+          <SearchForm tab={tab} setTab={setTab} />
+          <div className="w-full bg-white min-h-screen rounded-t-[32px] pt-10 px-4">
+            <Recommendations />
+          </div>
         </div>
       </div>
-    </div>
+    </NoSSR>
   );
 }
 
