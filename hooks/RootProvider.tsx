4e1bb8a4c99b97f004d7112a4968cb5b92592796
ayubxon13 <yuldoshevv7@gmail.com>
2024-07-23 22:54:48 +0500

    ssr ulandi

--- hooks/RootProvider.tsx
+++ hooks/RootProvider.tsx
@@ -1,15 +1,19 @@
-import React, { ReactNode } from 'react';
-import { PromoCodeProvider } from '@/hooks/PromoProvider';
+import React, {ReactNode} from "react";
+import {PromoCodeProvider} from "@/hooks/PromoProvider";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 interface AppProviderProps {
   children: ReactNode;
+  dehydratedState?: unknown;
 }
 
-const RootProvider: React.FC<AppProviderProps> = ({ children }) => {
+const RootProvider: React.FC<AppProviderProps> = ({children}) => {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
-    <PromoCodeProvider>
-    {children}
-    </PromoCodeProvider>
+    <QueryClientProvider client={queryClient}>
+      <PromoCodeProvider>{children}</PromoCodeProvider>
+    </QueryClientProvider>
   );
 };
 
